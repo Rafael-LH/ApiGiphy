c9bc465d8643dea99ee12a2ef31defe87f2c7f3f
Rafael Lopez <rafael.iskoat@gmail.com>
2020-09-28 21:37:37 -0500

    Agregamos infinite scroll

--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -1,28 +1,38 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-export default function useNearScreen({ distance = '100px' } = {}) {
-  const [isNearScreen, setIsNearScreen] = useState(false);
-  const refLazyLoad = useRef();
+export default function useNearScreen({ distance = '100px', externalRef, once = true } = {}) {
+  const [isNearScreen, setShow] = useState(false);
+  const fromRef = useRef();
 
   useEffect(() => {
+    let observer
+
+    const element = externalRef ? externalRef.current : fromRef.current
+
     const onChange = (entries, observer) => {
       const el = entries[0];
       console.log(el.isIntersecting)
       if (el.isIntersecting) {
-        setIsNearScreen(true);
+        setShow(true);
         // esto es en caso de que solo quiera escuchar solo un intersect y nada mas, lo que hacemos es desconectar el intersect
         // con la funcion disconnect()
-        observer.disconnect();
+        once && observer.disconnect();
+      } else {
+        !once && setShow(false)
       }
     }
-    // console.log(refLazyLoad.current)
-    const observer = new IntersectionObserver(onChange, {
-      rootMargin: distance
+    Promise.resolve(
+      typeof IntersectionObserver !== 'undefined'
+        ? IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new IntersectionObserver(onChange, {
+        rootMargin: distance
+      })
+      if (element) observer.observe(element)
     })
-    observer.observe(refLazyLoad.current)
-
-    return () => observer.disconnect()
+    return () => observer && observer.disconnect()
   })
 
-  return { isNearScreen, refLazyLoad }
+  return { isNearScreen, fromRef }
 }
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, { useRef, useEffect, useCallback } from 'react'
 import Spinner from '../../components/Spinner'
 import ListOfGifs from '../../components/ListOfGifs'
 import { useGifs } from '../../hooks/useGifs'
+import useNearScreen from '../../hooks/useNearScreen'
+import debounce from 'just-debounce-it'
 
 export default function SearchResults({ params }) {
   const { keyword } = params
   const { loading, gifs, setPage } = useGifs({ keyword })
+  const externalRef = useRef()
+  const { isNearScreen } = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false,
+  })
+  const handleNextPage = () => console.log('next page');
 
-  function handleNextPage(e) {
-    e.preventDefault();
-    setPage((prevPage) => prevPage + 1);
-  }
+  // el useCallback recibe una dependencia
+  // use callback retorna una funcion
+  const debounceHandleNextPage = useCallback(debounce(
+    () => setPage(prevPage => prevPage + 1), 500
+  ), [])
+
+  useEffect(() => {
+    console.log(isNearScreen)
+    if (isNearScreen) debounceHandleNextPage()
+  }, [debounceHandleNextPage, isNearScreen]) // dependencia de nuestro efecto
+
   return <>
     {loading
       ? <Spinner />
       : <>
         <h3 className="App-title">{keyword}</h3>
         <ListOfGifs gifs={gifs} />
+        <div id="visor" ref={externalRef}></div>
       </>
     }
-    <br />
-    <button onClick={handleNextPage}>Get Next Page</button>
   </>
 }
